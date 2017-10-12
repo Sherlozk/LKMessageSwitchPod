640c02a954f0d86161f564cf2ad3ef51d9e83d35
sherlock <sherlock@locker-imac.local>
2017-10-12 20:54:39 +0800

    Initial Commit

---
+++ LKMessageSwitchPod/LKMessageSwitchPod.h
@@ -0,0 +1,19 @@
+//
+//  LKMessageSwitchPod.h
+//  LKMessageSwitchPod
+//
+//  Created by sherlock on 2017/10/12.
+//  Copyright © 2017年 sherlock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LKMessageSwitchPod.
+FOUNDATION_EXPORT double LKMessageSwitchPodVersionNumber;
+
+//! Project version string for LKMessageSwitchPod.
+FOUNDATION_EXPORT const unsigned char LKMessageSwitchPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LKMessageSwitchPod/PublicHeader.h>
+
+
---
+++ LKMessageSwitchPod/LKMessageSwitchPod.m
@@ -0,0 +1,24 @@
+//  weibo: http://weibo.com/xiaoqing28
+//  blog:  http://www.alonemonkey.com
+//
+//  LKMessageSwitchPod.m
+//  LKMessageSwitchPod
+//
+//  Created by sherlock on 2017/10/12.
+//  Copyright (c) 2017年 sherlock. All rights reserved.
+//
+
+#import "LKMessageSwitchPod.h"
+#import "CaptainHook.h"
+#import <UIKit/UIKit.h>
+
+CHDeclareClass(CustomViewController)
+
+CHOptimizedMethod(0, self, NSString*, CustomViewController,getMyName){
+    return @"MonkeyDevPod";
+}
+
+CHConstructor{
+    CHLoadLateClass(CustomViewController);
+    CHClassHook(0, CustomViewController, getMyName);
+}
